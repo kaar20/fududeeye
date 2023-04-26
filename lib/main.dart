0947d154d5bfb2484ca407957a33d5c0e778d9cf
kaar20 <codewithkar@gmail.com>
2023-04-26 23:13:17 +0300

    fududeeye_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:alan_voice/alan_voice.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +17,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final FlutterTts ft = FlutterTts();
   static const _hover = const MethodChannel('kikoba.co.tz/hover');
   String _ActionResponse = 'Waiting for Response...';
+  // String _ActionRe
+  // String _ActionResponse = 'sucess';
   Future<dynamic> sendMoney(var number, money) async {
     var sendMap = <String, dynamic>{
       'number': number,
@@ -23,13 +29,24 @@ class _MyAppState extends State<MyApp> {
     };
 // response waits for result from java code
     String? response = "";
+    String me = "";
     try {
       final String? result = await _hover.invokeMethod('sendMoney', sendMap);
       response = result;
+      me = "You have correctly sent $money\$ to the number ${phone.text}. ";
     } on PlatformException catch (e) {
       response = "Failed to Invoke: '${e.message}'.";
+      me = "Failed Transaction";
     }
-    _ActionResponse = response!;
+    _ActionResponse = me;
+    setState(() {
+      // _ActionResponse = me!;
+      // _ActionResponse = me;
+      // // print(me)
+      // _ActionResponse = response!;
+
+      // if(response!)
+    });
   }
 
   final phone = TextEditingController();
@@ -47,90 +64,165 @@ class _MyAppState extends State<MyApp> {
     switch (command["command"]) {
       case "getphone":
         phone.text = command["text"].toString().replaceAll("-", "");
-        // var str="185-51-671";
-// var newStr = str.replace(/-/g, "");
         break;
       case "getmoney":
         money.text = command["text"];
+        break;
+      case "forward":
+        sendMoney("${phone.text}", "${money.text}");
+        break;
+      case "lastaction":
+        speak(_ActionResponse);
         break;
       default:
         debugPrint("Unknown command");
     }
   }
 
+  speak(String text) async {
+    await ft.setLanguage("en-US");
+    await ft.setPitch(1);
+    await ft.speak(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Fududeeye'),
-        ),
-        body: Center(
+        backgroundColor: Colors.white,
+        // appBar: AppBar(
+        //   title: const Text('Fududeeye'),
+        // ),
+        body: SafeArea(
           child: Center(
-              child: Form(
-                  child: Container(
             child: Column(
               children: [
                 Container(
-                  // padding: EdgeInsets.all(10),
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text(
-                      "................................................... "),
-                ),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  child: TextFormField(
-                    controller: phone,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "reciever phone"),
+                  padding: EdgeInsets.only(top: 10),
+                  // child: Image(image: AssetImage('images/img.png')),
+                  child: Image.asset(
+                    // "images/img.png",
+                    "images/logo.jpeg",
+                    // width: 200,
+                    // height: 210,
+
+                    // fit: BoxFit.fitHeight,
+                    // fit: BoxFit.fitWidth,
+
+                    scale: 2,
                   ),
                 ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  child: TextFormField(
-                    controller: money,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'amount of money'),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: SizedBox(
-                      height: 50,
-                      width: double.infinity,
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  sendMoney("${phone.text}", "${money.text}");
-                                },
-                                child: Text(
-                                  'Send',
-                                  style: TextStyle(fontSize: 20),
-                                )),
+                Form(
+                    child: Container(
+                  // padding: EdgeInsets.only(),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(
+                            bottom: 20, left: 20, right: 20, top: 10),
+                        child: TextFormField(
+                          controller: phone,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                    color: Colors.transparent, width: 0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Colors.transparent, width: 0)),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 21),
+                              filled: true,
+                              fillColor: Color(0xffEBEBEB),
+                              // fillColor: Color(0xffD9D9D9),
+                              hintText: 'Number Loo Diraha ',
+                              hintStyle: TextStyle(
+                                  // color: Color(0xff3F3C3C),
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600)),
+                        ),
+                      ),
+                      // SizedBox(
+                      //   height: 3,
+                      // ),
+                      Container(
+                        // padding: EdgeInsets.all(20),
+                        padding: EdgeInsets.only(
+                            bottom: 20, left: 20, right: 20, top: 10),
+                        child: TextFormField(
+                          controller: money,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(
+                                    color: Colors.transparent, width: 0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(
+                                      color: Colors.transparent, width: 0)),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 21),
+                              filled: true,
+                              fillColor: Color(0xffEBEBEB),
+                              // fillColor: Color(0xffD9D9D9),
+                              hintText: 'Cadad-ka lacagta',
+                              hintStyle: TextStyle(
+                                  // color: Color(0xff3F3C3C),
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600)),
+                          // decoration: InputDecoration(
+                          //     border: OutlineInputBorder(),
+                          //     labelText: 'amount of money'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: SizedBox(
+                          width: double.infinity,
+                          // width: 10,
+                          height: 55,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xff5717E2),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(52), // <-- Radius
+                                ),
+                              ),
+                              onPressed: () {
+                                sendMoney("${phone.text}", "${money.text}");
+                              },
+                              child: Text(
+                                'Send',
+                                style:
+                                    TextStyle(fontSize: 23, letterSpacing: 2),
+                              )),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _ActionResponse,
+                            style: TextStyle(fontSize: 16, letterSpacing: 1),
                           ),
-                          // Center(
-                          //   child: Text(_ActionResponse),
-                          // )
-                          // Text(_ActionResponse)
-                        ],
-                      )),
-                ),
-                Center(
-                  child: Text(_ActionResponse),
-                )
+                        ),
+                      )
+                    ],
+                  ),
+                )),
               ],
             ),
-          ))),
+          ),
         ),
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_tts
 import package_info
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
 }
